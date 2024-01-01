f74e16bbfa1a93adb84917cab49f3020b361ed75
Joma <117911315+CalvinAX@users.noreply.github.com>
2024-01-01 21:00:35 +0100

    Tag Getter and Setter

--- src/main/java/com/referatapi/app/api/model/Tag.java
+++ src/main/java/com/referatapi/app/api/model/Tag.java
@@ -11,4 +11,38 @@ public class Tag {
     private int id;
     private String name;
     private String color;
+
+    public Tag(int id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Tag() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
